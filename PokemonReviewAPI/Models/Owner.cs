93485378c35ec09691978adf88a9eee5ca7696c5
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-11-14 23:16:45 +0100

    Add project files.

---
+++ PokemonReviewAPI/Models/Owner.cs
@@ -0,0 +1,7 @@
+﻿namespace PokemonReviewAPI.Models {
+    public class Owner {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
---
+++ PokemonReviewAPI/Models/Review.cs
@@ -0,0 +1,7 @@
+﻿namespace PokemonReviewAPI.Models {
+    public class Review {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
